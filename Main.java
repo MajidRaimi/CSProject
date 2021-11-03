ce4c846072a3dd1c277687740bf074e3814aa785
WeeboVoid <if9ool.genius@gmail.com>
2021-11-03 15:47:11 +0300

    Bakhurji

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Commit From Majid");
+        System.out.println("Commit From Majid the shit");
     }
 }
